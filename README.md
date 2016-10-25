7e1d6bdd90a8b7646acd0b15d906631ab81d16e0
artloric <jonathantager@outlook.com>
2016-10-25 16:10:20 +0200

    V 0.0.4 upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# cordova-plugin-biometricscan
+Custom Biometric Plugin for Android-Cordova
+
+#25/10/2016
+This plugin is challengeing to say the least. 
---
+++ www/biometric.js
@@ -0,0 +1,26 @@
+/*
+With these plugins, we're defining a JS interface, to call a cordova.exec function, which will travel over the webView brige,
+to the native side.
+There it will look for our custom class.
+We overwrite one of the exec functions inherited by the extension of a cordova plugin class and pass it our own class and method actions.
+*/
+window.bioscan = function(str) {
+	
+	cordova.exec(function(message) {
+		console.log('String back from Java class:' + message);
+	},
+
+		function(error) {
+			callback(message);
+			console.log("I am a companion negation confirmer. Exec method did not complete");
+		},
+             //Java class with method in-contained
+             "Bioservice",
+             //Method of Java class to be called
+             "scan",
+             //Bunch of arguments to be passed to the Native side
+             [str]);
+}
+
+var win = window;
+win.onload = win.bioscan("Biometric");
